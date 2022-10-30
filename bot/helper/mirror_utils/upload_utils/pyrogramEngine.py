8a739538de3357336d30a3eb945646ad20ac7254
Dinuka Sandeepa <60549728+DinukaSandeepa@users.noreply.github.com>
2022-10-30 19:46:36 +0530

    Update pyrogramEngine.py

--- bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
+++ bot/helper/mirror_utils/upload_utils/pyrogramEngine.py
@@ -86,20 +86,20 @@ class TgUploader:
             if file_.startswith('www'):
                 file_ = ' '.join(file_.split()[1:])
                 file_ = f"{PRENAME_X}" + file_.strip('-').strip('_')
-                cap_mono = f"<b>{CAPTION_FONT}>{file_}</b>{CAPTION_FONT}>"
+                cap_mono = f"<b>{CAPTION_FONT}>{file_}</b>{CAPTION_FONT}"
                 cap = f"\n\n{CAPTION_X}\n\n"
                 new_path = ospath.join(dirpath, file_)
                 osrename(up_path, new_path)
                 up_path = new_path
             else:
                 file_ = f"{PRENAME_X}" + " " + file_.strip('-').strip('_')
-                cap_mono = f"<b>{CAPTION_FONT}>{file_}</b>{CAPTION_FONT}>"
+                cap_mono = f"<b>{CAPTION_FONT}>{file_}</b>{CAPTION_FONT}"
                 cap = f"\n\n{CAPTION_X}\n\n"
                 new_path = ospath.join(dirpath, file_)
                 osrename(up_path, new_path)
                 up_path = new_path
         else:
-            cap_mono = f"<b>{CAPTION_FONT}>{file_}</b>{CAPTION_FONT}>"
+            cap_mono = f"<b>{CAPTION_FONT}>{file_}</b>{CAPTION_FONT}"
             cap = f"\n\n{CAPTION_X}\n\n"
         # if CUSTOM_FILENAME is not None and prefix == '':
         #     cap_mono = f"<{CAPTION_FONT}>{CUSTOM_FILENAME} {file_}</{CAPTION_FONT}>"
